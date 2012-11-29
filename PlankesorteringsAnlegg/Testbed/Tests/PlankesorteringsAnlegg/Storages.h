bfedfe5ed684b66c1ef3d7a52b39f02621b7f22d
roy <roy@ubuntu-oneric>
2012-11-30 00:56:52 +0100

    add storagearea class
    This class is used to easily change storage-count and dimensions.

---
+++ PlankesorteringsAnlegg/Testbed/Tests/PlankesorteringsAnlegg/Storages.h
@@ -0,0 +1,144 @@
+/*
+ * Lomme.h
+ *
+ *  Created on: 27. nov. 2012
+ *      Author: roy
+ */
+
+#ifndef STORAGES_H_
+#define STORAGES_H_
+
+#include <vector>
+
+class Storage{
+public:
+	Storage(b2Vec2 upperLeftCorner,float32 width,float32 height, float32 wallThickness,b2World* world){
+		b2BodyDef bd;
+		bd.type=b2_staticBody;
+		bd.position=upperLeftCorner;
+		b2FixtureDef fd;
+		b2PolygonShape shape;
+		//left wall:
+		shape.SetAsBox(wallThickness/2,height/2,b2Vec2(wallThickness/2,-height/2),0);
+		fd.shape=&shape;
+		b2Body* bodyStorage=world->CreateBody(&bd);
+		bodyStorage->CreateFixture(&fd);
+		//right wall:
+		shape.SetAsBox(wallThickness/2,height/2,b2Vec2(width-wallThickness/2,-height/2),0);
+		fd.shape=&shape;
+		bodyStorage->CreateFixture(&fd);
+
+		//lift:
+		bd.type=b2_dynamicBody;
+		bd.gravityScale=0.0f;
+		bd.position=upperLeftCorner+b2Vec2(width/2,-wallThickness/2);
+		b2Body* bodyLift=world->CreateBody(&bd);
+		fd.density=0.1f;
+		fd.restitution=0.0f;
+		shape.SetAsBox(width/2-wallThickness*1.1,wallThickness/2);
+		fd.shape=&shape;
+		bodyLift->CreateFixture(&fd);
+
+		//lift joint:
+		b2PrismaticJointDef pjd;
+		pjd.bodyA=bodyLift;
+		pjd.bodyB=bodyStorage;
+		pjd.localAnchorA=b2Vec2(0,0);
+		pjd.localAnchorB=b2Vec2(width/2,-wallThickness/2);
+		pjd.localAxisA=b2Vec2(0,1);
+
+		pjd.enableLimit=true;
+		pjd.lowerTranslation=0.0f;
+		pjd.upperTranslation=12.0f;
+
+		pjd.enableMotor=true;
+		pjd.motorSpeed=0;
+		pjd.maxMotorForce=100;
+
+		m_jointLift=(b2PrismaticJoint*)world->CreateJoint(&pjd);
+
+		//stopper:
+		bd.type=b2_dynamicBody;
+		bd.gravityScale=0.0f;
+		bd.position=upperLeftCorner+b2Vec2(width/2,3.0f);
+		b2Body* m_bodyStopper=world->CreateBody(&bd);
+		fd.density=0.1f;
+		fd.restitution=0.0f;
+		shape.SetAsBox(0.125f,1.0f);
+		fd.shape=&shape;
+		m_bodyStopper->CreateFixture(&fd);
+
+		//stopper joint:
+		pjd.bodyA=m_bodyStopper;
+		pjd.bodyB=bodyStorage;
+		pjd.localAnchorA=b2Vec2(0,0);
+		pjd.localAnchorB=b2Vec2(width/2,3.0f);
+		pjd.localAxisA=b2Vec2(0,1);
+
+		pjd.lowerTranslation=0.0f;
+		pjd.upperTranslation=1.5f;
+
+		m_jointStopper=(b2PrismaticJoint*)world->CreateJoint(&pjd);
+
+		//top-sensor-fixture:
+		fd.isSensor=true;
+		shape.SetAsBox(width/2-wallThickness,wallThickness/16,b2Vec2(width/2,-wallThickness/16),0);
+		fd.shape=&shape;
+		m_fixtureTopSensor=bodyStorage->CreateFixture(&fd);
+
+		//middle-sensor-fixture:
+		fd.isSensor=true;
+		shape.SetAsBox(width/2-wallThickness,wallThickness/16,b2Vec2(width/2,-height+wallThickness/16),0);
+		fd.shape=&shape;
+		m_fixtureMiddleSensor=bodyStorage->CreateFixture(&fd);
+
+		//bottom-sensor-fixture:
+		fd.isSensor=true;
+		shape.SetAsBox(width/2-wallThickness,wallThickness/16,b2Vec2(width/2,-12.0f-wallThickness),0);
+		fd.shape=&shape;
+		m_fixtureBottomSensor=bodyStorage->CreateFixture(&fd);
+	}
+	b2PrismaticJoint* getLiftJoint(){
+		return m_jointLift;
+	}
+	b2PrismaticJoint* getStopperJoint(){
+		return m_jointStopper;
+	}
+	b2Fixture* getTopSensorFixture(){
+		return m_fixtureTopSensor;
+	}
+	b2Fixture* getMiddleSensorFixture(){
+		return m_fixtureMiddleSensor;
+	}
+	b2Fixture* getBottomSensorFixture(){
+		return m_fixtureBottomSensor;
+	}
+
+	b2Body* m_bodyStopper;
+	b2PrismaticJoint* m_jointLift;
+	b2PrismaticJoint* m_jointStopper;
+	b2Fixture* m_fixtureTopSensor;
+	b2Fixture* m_fixtureMiddleSensor;
+	b2Fixture* m_fixtureBottomSensor;
+};
+
+class StorageArea{
+public:
+	StorageArea(int numberOfStorages,b2Vec2 upperLeftCorner,float32 width,float32 height, float32 wallThickness,b2World* world){
+		b2Vec2 tmpPos=upperLeftCorner;
+		b2Vec2 vecDisplace=b2Vec2(width,0);
+		for(int i=0;i<numberOfStorages;i++){
+			m_storage.push_back(new Storage(tmpPos,width,height,wallThickness,world));
+			tmpPos+=vecDisplace;
+		}
+	}
+	b2PrismaticJoint* getLiftJoint(int i){
+		return m_storage.at(i)->getLiftJoint();
+	}
+	b2PrismaticJoint* getStopperJoint(int i){
+		return m_storage.at(i)->getStopperJoint();
+	}
+	vector<Storage*> m_storage;
+};
+
+#endif /* STORAGES_H_ */
