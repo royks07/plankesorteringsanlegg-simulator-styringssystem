b0f9660bbc3a78f71d24a394b39bdb95c1f165fd
roy <roy@ubuntu-oneric>
2012-12-02 15:54:39 +0100

    add PackageOutput
    This class constructs the fork, stopperbeam and lift at the end of the
    line.

--- PlankesorteringsAnlegg/Testbed/Tests/PlankesorteringsAnlegg/Packaging.h
+++ PlankesorteringsAnlegg/Testbed/Tests/PlankesorteringsAnlegg/Packaging.h
@@ -86,5 +86,129 @@ public:
 	b2RevoluteJoint* m_jointRotate;
 };
 
+class PackageOutput{
+public:
+	PackageOutput(b2Vec2 position,b2World* world){
+		float32 floorWidth=19.0f;
+		float32 floorThickness=1.0f;
+		float32 stopperBeamLength=6.8f;
+		float32 stopperBeamThickness=0.4f;
+		float32 forkBeamThickness=1.0f;
+		float32 forkBaseLength=1.6f;
+		float32 forkBeamLength=3.4f;
+		float32 forkThickness=0.25f;
+		float32 liftThickness=1.0f;
+		float32 x=(floorWidth-forkBeamThickness)/2;
+
+		//FLOOR-BEAM
+		b2BodyDef bd;
+		bd.position=position;
+		bd.type=b2_staticBody;
+		bd.fixedRotation=true;
+
+		b2PolygonShape shape;
+		shape.SetAsBox(floorWidth/2,floorThickness/2);
+
+		b2FixtureDef fd;
+		fd.restitution=0;
+		fd.friction=100;
+		fd.density=100;
+		fd.shape=&shape;
+
+		b2Body* body=world->CreateBody(&bd);
+		body->CreateFixture(&fd);
+
+		//STANDING BEAM
+		shape.SetAsBox(stopperBeamThickness/2,stopperBeamLength/2,b2Vec2(-floorWidth/2+x-stopperBeamThickness/2,floorThickness/2+stopperBeamLength/2),0);
+		fd.shape=&shape;
+		fd.filter.groupIndex=-3;
+		body->CreateFixture(&fd);
+
+		//LIFT
+		bd.position=position+b2Vec2(-floorWidth/2+(x-stopperBeamThickness)/2,floorThickness/2+liftThickness/2);
+		bd.type=b2_dynamicBody;
+		b2Body* bodyLift=world->CreateBody(&bd);
+		shape.SetAsBox((x-stopperBeamThickness)/2,liftThickness/2);
+		fd.shape=&shape;
+		fd.filter.groupIndex=0;
+
+		bodyLift->CreateFixture(&fd);
+
+		//FORK
+		bd.position=position+b2Vec2(floorWidth/2-forkBeamThickness/2,floorThickness/2+stopperBeamLength-forkThickness-forkBeamLength/2);
+		bd.type=b2_dynamicBody;
+
+		b2Body* bodyFork=world->CreateBody(&bd);
+		shape.SetAsBox(forkBeamThickness/2,forkBeamLength/2);
+		fd.shape=&shape;
+		fd.filter.groupIndex=-3;
+		bodyFork->CreateFixture(&fd);
+
+		shape.SetAsBox((x+forkBeamThickness)/2,forkThickness/2,b2Vec2(-(x+forkBeamThickness)/2+forkBeamThickness/2,forkBeamLength/2+forkThickness/2),0);
+		fd.shape=&shape;
+		bodyFork->CreateFixture(&fd);
+
+		//FORKBASE
+		bd.position=position+b2Vec2(floorWidth/2-forkBeamThickness/2,floorThickness/2+forkBaseLength/2);
+		bd.type=b2_dynamicBody;
+
+		shape.SetAsBox(forkBeamThickness/2,forkBaseLength/2);
+		fd.shape=&shape;
+		fd.filter.groupIndex=0;
+
+		b2Body* bodyForkBase=world->CreateBody(&bd);
+		bodyForkBase->CreateFixture(&fd);
+
+		//JOINT FORKBASE - "FRAME"
+		b2PrismaticJointDef pjd;
+		pjd.bodyA=bodyForkBase;
+		pjd.bodyB=body;
+		pjd.localAxisA=b2Vec2(1,0);
+		pjd.localAnchorB=b2Vec2(floorWidth/2-forkBeamThickness/2,floorThickness/2+forkBaseLength/2);
+		pjd.enableLimit=true;
+		pjd.upperTranslation=x;
+		pjd.lowerTranslation=0;
+		pjd.enableMotor=true;
+		pjd.maxMotorForce=10000;
+		m_jointForkBase=(b2PrismaticJoint*)world->CreateJoint(&pjd);
+
+		//JOINT FORK - FORKBASE
+		pjd.bodyA=bodyFork;
+		pjd.bodyB=bodyForkBase;
+		pjd.localAxisA=b2Vec2(0,1);
+		pjd.localAnchorB=b2Vec2(0,stopperBeamLength-forkThickness-forkBeamLength/2-forkBaseLength/2);
+
+		pjd.upperTranslation=1;
+		pjd.lowerTranslation=-forkThickness;
+
+		//pjd.collideConnected=true;
+		m_jointFork=(b2PrismaticJoint*)world->CreateJoint(&pjd);
+
+		//JOINT LIFT - "FRAME"
+		pjd.bodyA=bodyLift;
+		pjd.bodyB=body;
+		pjd.localAxisA=b2Vec2(0,1);
+		pjd.localAnchorB=b2Vec2(-floorWidth/2+(x-stopperBeamThickness)/2,floorThickness/2+liftThickness/2);
+
+		pjd.upperTranslation=0;
+		pjd.lowerTranslation=-(stopperBeamLength-liftThickness);
+
+		//pjd.collideConnected=true;
+		m_jointLift=(b2PrismaticJoint*)world->CreateJoint(&pjd);
+	}
+	b2PrismaticJoint* getForkJoint(){
+		return m_jointFork;
+	}
+	b2PrismaticJoint* getForkBaseJoint(){
+		return m_jointForkBase;
+	}
+	b2PrismaticJoint* getLiftJoint(){
+		return m_jointLift;
+	}
+
+	b2PrismaticJoint* m_jointForkBase;
+	b2PrismaticJoint* m_jointFork;
+	b2PrismaticJoint* m_jointLift;
+};
 
 #endif /* PACKAGING_H_ */
